920da267b7881568c875eb09b563b8cb9d96ebe6
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-10 20:38:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AppLogicaExerciciosPropostos_01
+{
+    /// <summary>
+    /// Exercícios sobre Estrutura Condicional (if-else)
+    /// Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+    /// </summary>
+    class Program
+    {
+        public static string ValidarNumero(int numero) 
+        {
+            string resultado = "";
+            if (numero<0)
+            {
+                resultado = "Negativo";
+            }
+            else
+            {
+                resultado = "Não Negativo";
+            }
+
+            return resultado;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor de um número: ");
+            string resultado = ValidarNumero(int.Parse(Console.ReadLine()));
+            Console.Clear();
+            Console.WriteLine("O seu número é: {0}", resultado);
+            Console.ReadLine();
+        }
+    }
+}
